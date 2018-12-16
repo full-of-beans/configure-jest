22cafe8dc596e69d6120ca71e4221644708933dd
iamoperand <narora200@gmail.com>
2018-12-16 19:03:19 +0530

    Setup an afterEach Test Hook for all tests with Jest setupTestFrameworkScriptFile

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -1,11 +1,7 @@
-import 'react-testing-library/cleanup-after-each';
 import React from 'react';
 import {render} from 'react-testing-library';
-import {createSerializer} from 'jest-emotion';
-import * as emotion from 'emotion';
-import CalculatorDisplay from '../calculator-display';
 
-expect.addSnapshotSerializer(createSerializer(emotion));
+import CalculatorDisplay from '../calculator-display';
 
 test('mounts', () => {
   const {container} = render(<CalculatorDisplay value="0" />);
