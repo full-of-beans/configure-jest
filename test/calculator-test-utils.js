7bf0e8d00ed76d14ae9154fa8b9f366a2bbd3358
iamoperand <narora200@gmail.com>
2018-12-17 13:40:29 +0530

    Support a test utilities file with Jest moduleDirectories

---
+++ test/calculator-test-utils.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'react-testing-library';
+import {ThemeProvider} from 'emotion-theming';
+import {dark} from '../src/themes';
+
+const renderWithProviders = (ui, options) => {
+  return render(<ThemeProvider theme={dark}>{ui}</ThemeProvider>, options);
+};
+
+export * from 'react-testing-library';
+export {renderWithProviders as render};
