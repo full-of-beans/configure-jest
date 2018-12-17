c190521dc87af3911457dcc5c943e327a4a70bd8
iamoperand <narora200@gmail.com>
2018-12-17 17:33:29 +0530

    Support Running Multiple Configurations with Jest’s Projects Feature

--- test/jest-common.js
+++ test/jest-common.js
@@ -12,8 +12,6 @@ module.exports = {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./style-mock.js'),
   },
-  collectCoverageFrom: ['**/src/**/*.js'],
-
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
--- test/jest.client.js
+++ test/jest.client.js
@@ -1,22 +1,9 @@
 module.exports = {
   ...require('./jest-common'),
   testEnvironment: 'jest-environment-jsdom',
+  displayName: 'client',
   // before Jest is loaded
   // setupFiles: [],
   // after Jest is loaded
   setupTestFrameworkScriptFile: require.resolve('./setup-tests.js'),
-  coverageThreshold: {
-    global: {
-      statements: 17,
-      branches: 4,
-      lines: 17,
-      functions: 20,
-    },
-    './src/shared/utils.js': {
-      statements: 100,
-      branches: 80,
-      lines: 100,
-      functions: 100,
-    },
-  },
 };
